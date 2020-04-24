401d6d5412eb406f32a9493dcb7bcb5a6370cc24
nikolina-marinova3 <56189511+nikolina-marinova3@users.noreply.github.com>
2020-04-24 14:33:07 +0100

    initial commit

---
+++ mailer.php
@@ -0,0 +1,26 @@
+<?php
+
+    // Get the form fields, removes html tags and removes the whitespace entered by the customer.
+    $name = strip_tags(trim($_POST["name"]));
+    $name = str_replace(array("\r","\n"),array(" "," "),$name);
+    $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+    $message = trim($_POST["choose-project"]);
+
+    // email address.
+    $recipient = "nikolina_marinova@yahoo.com";
+
+    // email subject.
+    $subject = "New contact from $name";
+
+    // email content.
+    $email_content = "Name: $name\n";
+    $email_content .= "Email: $email\n\n";
+    $email_content .= "Message:\n$message\n";
+
+    // Build the email headers.
+    $email_headers = "From: $name <$email>";
+
+    // Send the email.
+    mail($recipient, $subject, $email_content, $email_headers);
+    
+?>
---
+++ primary/js/script-original.js
@@ -0,0 +1,113 @@
+// SHOW MOBILE MENU
+// immediately invoked function expression
+
+
+(function() {
+  const menuInfo = document.getElementById("menu-info");
+  const menuInfoClose = document.getElementById("menu-info-close");
+  const menu = document.getElementById("mobile-menu");
+    
+  // show menu
+  menuInfo.addEventListener("click", function() {
+    menu.classList.toggle("show-menu");
+  });
+  // close menu
+  menuInfoClose.addEventListener("click", function() {
+    menu.classList.toggle("show-menu"); 
+  });
+})();
+
+// SHOW OFFERs CONTAINER
+// immediately invoked function expression
+
+
+(function() {
+  const viewOffers = document.getElementById("view-offers");
+  const closeOffer = document.getElementById("close-offer");
+  const container = document.getElementById("container");
+    
+  // show offers container
+  viewOffers.addEventListener("click", function() {
+    container.classList.toggle("show-container");
+  });
+  // close offers container
+    closeOffer.addEventListener("click", function() {
+    container.classList.toggle("show-container"); 
+  });
+})();
+
+
+
+// PRICE and OFFERS - offers scroller
+
+const data = [
+  {
+    number: '1 from 3',
+    name: '10% discount on contemporary patio and garden furniture.',
+    description: 'Furniture with a 7-year guarantee available in five weave colours. Rattan all-weather furniture is durable in the harshest sunlight and the coldest winters and can be left outside all year round.',
+    image: 'primary/img/7_1_gallery.jpg',
+    alt: '10% discount on patio and garden furniture'
+  },
+  {
+    number: '2 from 3',
+    name: '10% discount on tall antiquated lighting for driveways.',
+    description: 'Choose from a variety of electrical lighting that is durable and provides greater illumination. Mark your passage safely and in style with our precisely placed driveway lights.',
+    image: 'primary/img/7_7_gallery.jpg',
+    alt: '10% discount on antiquated lighting for driveways'
+  },
+  {
+    number: '3 from 3',
+    name: '15% discount on plant fragrant flowers for your garden or yard.',
+    description: 'Create a succession of seasonal aromatherapies with our garden fragrant flowers and bushes in order to maximise the experience of you and your guests.',
+    image: 'primary/img/7_2_gallery.jpg',
+    alt: '15% discount on plant fragrant flowers for your garden or yard'
+  },
+];
+
+const offers = offerIterator(data);
+
+// Call first offer
+nextOffer();
+
+// Next Event
+document.getElementById('next').addEventListener('click', nextOffer);
+
+// Next offer Display
+function nextOffer() {
+  const currentOffer = offers.next().value;
+
+  if(currentOffer !== undefined) {
+    document.getElementById('offerDisplay').innerHTML = `
+    <ul class="list-group">
+        <li class="list-group-item">Offer Number: ${currentOffer.number}</li>
+        <li class="list-group-item">Title: ${currentOffer.name}</li>
+        <li class="list-group-item">Description: ${currentOffer.description}</li>
+      </ul>
+    `;
+
+    document.getElementById('imageDisplay').innerHTML = `<img src="${currentOffer.image}" alt="${currentOffer.alt}">`;
+  } 
+    
+      
+    else {
+    // No more offers
+    window.description.reload();
+        
+    function reloadThePage(){
+    window.location.reload();
+        } 
+  }
+}
+
+// offer Iterator
+function offerIterator(offers) {
+  let nextIndex = 0;
+
+  return {
+    next: function() {
+      return nextIndex < offers.length ? 
+      { value: offers[nextIndex++], done: false } : 
+      { done: true }
+    }
+  };
+}
